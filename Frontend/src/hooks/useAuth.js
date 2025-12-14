77287147c472a92b9751a1f5f7d86dbe0e6fa865
saumyaunb007 <saumyasrivastava@unbundl.com>
2025-12-14 23:02:46 +0530

    add baseURL

--- Frontend/src/hooks/useAuth.js
+++ Frontend/src/hooks/useAuth.js
@@ -1,10 +1,13 @@
 import axios from "axios";
 
 export const api = axios.create({
-  baseURL: "http://localhost:5000",
+  baseURL: import.meta.env.VITE_API_URL,
 });
 
 export const setAuthToken = (token) => {
-  if (token) api.defaults.headers.common["Authorization"] = `Bearer ${token}`;
-  else delete api.defaults.headers.common["Authorization"];
+  if (token) {
+    api.defaults.headers.common["Authorization"] = `Bearer ${token}`;
+  } else {
+    delete api.defaults.headers.common["Authorization"];
+  }
 };
